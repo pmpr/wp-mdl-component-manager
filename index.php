c7835d5ff660479160fddb926b671da5775ed190
Pmnmpr <pmnmpr@gmail.com>
2021-12-21 16:11:24 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61c066a214e74             |
+    |             61c1fc8b40e63             |
     |_______________________________________|
 */
  use Pmpr\Package\ComponentManager\ComponentManager; ComponentManager::symcgieuakksimmu();
--- src/Pmpr/Package/ComponentManager/Salary.php
+++ src/Pmpr/Package/ComponentManager/Salary.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61c066a214e74             |
+    |             61c1fc8b40e63             |
     |_______________________________________|
 */
- namespace Pmpr\Package\ComponentManager; use Pmpr\Package\Salary\AbstractSalary; class Salary extends AbstractSalary { public function __construct() { $this->title = __("\103\157\155\160\157\x6e\145\x6e\164\40\115\x61\x6e\x61\x67\145\x72\40\x4d\x6f\x64\x75\x6c\x65", PR__PKG__COMPONENT_MANAGER); parent::__construct(); } public function kqceciiyqweskgac($mkucggyaiaukqoce, $xssuewsokckmigqk) : bool { return true; } }
+ namespace Pmpr\Package\ComponentManager; use Pmpr\Package\Salary\AbstractSalary; class Salary extends AbstractSalary { public function __construct() { $this->title = __("\103\x6f\x6d\160\x6f\x6e\145\x6e\164\40\115\141\156\141\147\x65\x72\x20\x4d\157\x64\x75\x6c\145", PR__PKG__COMPONENT_MANAGER); parent::__construct(); } public function kqceciiyqweskgac($mkucggyaiaukqoce, $xssuewsokckmigqk) : bool { return true; } }
